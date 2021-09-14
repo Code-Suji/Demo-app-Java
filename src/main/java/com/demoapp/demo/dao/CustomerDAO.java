d4ad6afb793d9bb2ba36bf29185d1aaad1f5ef96
Code-Suji <sujithcssujith008@gmail.com>
2021-09-14 14:44:07 +0530

    Updated after defining crud with db

--- src/main/java/com/demoapp/demo/dao/CustomerDAO.java
+++ src/main/java/com/demoapp/demo/dao/CustomerDAO.java
@@ -1,2 +1,13 @@
-package com.demoapp.demo.dao;public interface CustomerDAO {
+package com.demoapp.demo.dao;
+
+import com.demoapp.demo.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerDAO extends CrudRepository<Customer,Integer> {
+    @Override
+    List<Customer> findAll();
 }
--- src/main/java/com/demoapp/demo/service/CustomerService.java
+++ src/main/java/com/demoapp/demo/service/CustomerService.java
@@ -1,6 +1,8 @@
 package com.demoapp.demo.service;
 
+import com.demoapp.demo.dao.CustomerDAO;
 import com.demoapp.demo.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PutMapping;
 
@@ -9,51 +11,59 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 @Component
 public class CustomerService {
+    @Autowired
+    private CustomerDAO customerDAO;
     private int customerIdCount = 1;
     private List<Customer> customerList = new CopyOnWriteArrayList<>();
 
     public Customer addCustomer(Customer customer){
-        customer.setCustomerId(customerIdCount);
-        customerList.add(customer);
-        customerIdCount++;
-        return customer;
+//        customer.setCustomerId(customerIdCount);
+//        customerList.add(customer);
+//        customerIdCount++;
+//        return customer;
+        return customerDAO.save(customer);
     }
 
     public List<Customer> getCustomers(){
-        return customerList;
+        return customerDAO.findAll();
+//        return customerList;
     }
 
     public Customer getCustomer(int customerId){
-        return  customerList
-                .stream()
-                .filter(c -> c.getCustomerId() == customerId)
-                .findFirst()
-                .get();
+//        return  customerList
+//                .stream()
+//                .filter(c -> c.getCustomerId() == customerId)
+//                .findFirst()
+//                .get();
+        return customerDAO.findById(customerId).get();
     }
     public Customer updateCustomer(int customerId, Customer customer){
-        customerList
-                .stream()
-                .forEach(c ->{
-                    if(c.getCustomerId() == customerId){
-                        c.setCustomerFirstname(customer.getCustomerFirstname());
-                        c.setCustomerLastname(customer.getCustomerLastname());
-                        c.setCustomerEmail(customer.getCustomerEmail());
-                    }
-                });
-        return customerList
-                .stream()
-                .filter(c-> c.getCustomerId() == customerId)
-                .findFirst()
-                .get();
+//        customerList
+//                .stream()
+//                .forEach(c ->{
+//                    if(c.getCustomerId() == customerId){
+//                        c.setCustomerFirstname(customer.getCustomerFirstname());
+//                        c.setCustomerLastname(customer.getCustomerLastname());
+//                        c.setCustomerEmail(customer.getCustomerEmail());
+//                    }
+//                });
+//        return customerList
+//                .stream()
+//                .filter(c-> c.getCustomerId() == customerId)
+//                .findFirst()
+//                .get();
+        customer.setCustomerId(customerId);
+        return customerDAO.save(customer);
     }
     public void deleteCustomer(int customerId){
-        customerList
-                .stream()
-                .forEach( c->{
-                    if(c.getCustomerId()==customerId){
-                        customerList.remove(c);
-                    }
-                });
+//        customerList
+//                .stream()
+//                .forEach( c->{
+//                    if(c.getCustomerId()==customerId){
+//                        customerList.remove(c);
+//                    }
+//                });
+        customerDAO.deleteById(customerId);
     }
 
 
